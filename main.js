31307651732d57ee49b5060c2c9246ce2756a02a
Ankit <ankit0839483@gmail.com>
2024-10-08 23:24:28 +0530

    responsive

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let isTypingForward = true;
 function typeText() {
   const sentence = sentences[currentSentence];
   if (isTypingForward) {
-    typingText.innerHTML = sentence.substring(0, index++) + "<span style='color:#fff; font-weight: 700;'>|</span>";
+    typingText.innerHTML = sentence.substring(0, index++) + "<span style='color:#fff;ma-left:-10px; font-weight: 700;'>|</span>";
     if (index > sentence.length + 1) {
       isTypingForward = false;
       setTimeout(typeText, 500);
@@ -15,7 +15,7 @@ function typeText() {
       setTimeout(typeText, 90);
     }
   } else {
-    typingText.innerHTML = sentence.substring(0, index--) + "<span style='color:#fff; font-weight: 700;'>|</span>";
+    typingText.innerHTML = sentence.substring(0, index--) + "<span style='color:#fff;padding-left:-10px; font-weight: 700;'>|</span>";
     if (index === -1) {
       currentSentence = (currentSentence + 1) % sentences.length;
       isTypingForward = true;
